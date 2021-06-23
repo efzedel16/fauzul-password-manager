e91002684978048e36686a227c362c1d81090a30
efzedel16 <efzedel16@outlook.com>
2021-06-23 23:06:50 +0700

    Fix password entity

--- server/entities/password.go
+++ server/entities/password.go
@@ -5,9 +5,9 @@ import "time"
 type Password struct {
 	Id        int       `json:"id" gorm:"primaryKey"`
 	UserId    int       `json:"user_id"`
-	Website   string    `json:"web"`
-	Password  string    `json:"password"`
+	Web       string    `json:"web"`
+	Pass      string    `json:"pass_hash"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	//DeletedAt time.Time `json:"deleted_at" gorm:"index"`
+	DeletedAt time.Time `json:"deleted_at" gorm:"index"`
 }
