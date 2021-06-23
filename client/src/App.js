481af905aa98f83e989c73ba5dfb63a945c915c2
efzedel16 <efzedel16@outlook.com>
2021-06-23 16:28:53 +0700

    Fix & add sign in route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import SignUpPage from "./pages/SignUpPage";
+import SignUp from "./components/pages/SignUp";
+import SignIn from "./components/pages/SignIn";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route component={SignUpPage} exact path="/user/signup" />
+        <Route component={SignUp} exact path="/signup" />
+        <Route component={SignIn} exact path="/signin" />
       </Switch>
     </Router>
   );
