d26b07c4cdc9d533acc02d7dc0c2eb7284abd83a
efzedel16 <efzedel16@outlook.com>
2021-06-23 23:04:02 +0700

    Create middleware

---
+++ server/handlers/authMiddle.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"FauzulPasswordManager/auth"
+	"FauzulPasswordManager/services"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func AuthMiddle(authService auth.AuthService, userService services.UserService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		header := c.GetHeader("Authorization")
+
+		token, err := authService.Validation(header)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorize user"})
+			return
+		}
+
+		claim, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorize user"})
+			return
+		}
+
+		id := int(claim["user_id"].(float64))
+		current, err := userService.GetById(id)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorize user"})
+			return
+		}
+
+		c.Set("current_user", current)
+	}
+
+}
