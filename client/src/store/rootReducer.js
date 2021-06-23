a6f27f7e0d32b3ce1557b63e4dbf7a6a4d0f3505
efzedel16 <efzedel16@outlook.com>
2021-06-24 03:19:52 +0700

    Add PassApp reducer

--- client/src/store/rootReducer.js
+++ client/src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
-import userReducer from "./reducer/userReducer";
+import user from "./reducer/userReducer";
+import pass from "./reducer/passReducer";
 
 const rootReducer = combineReducers({
-  user: userReducer,
+  user,
+  pass,
 });
 
 export default rootReducer;
