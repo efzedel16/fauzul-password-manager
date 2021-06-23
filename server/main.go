eed3ebfe1004ba5acb05592e1c3c3a70587364f5
efzedel16 <efzedel16@outlook.com>
2021-06-23 17:13:08 +0700

    CORS clean code

--- server/main.go
+++ server/main.go
@@ -1,33 +1,18 @@
 package main
 
 import (
-  "FauzulPasswordManager/routes"
-  "github.com/gin-gonic/gin"
+	"FauzulPasswordManager/auth"
+	"FauzulPasswordManager/routes"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  r := gin.Default()
-  r.Use(CORSMiddleware())
+	r := gin.Default()
+	r.Use(auth.CORSMiddleware())
 
-  routes.User(r)
+	routes.User(r)
 
-  if err := r.Run(); err != nil {
-    return
-  }
-}
-
-func CORSMiddleware() gin.HandlerFunc {
-  return func(c *gin.Context) {
-    c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-    c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-    c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-    c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
-
-    if c.Request.Method == "OPTIONS" {
-      c.AbortWithStatus(204)
-      return
-    }
-
-    c.Next()
-  }
+	if err := r.Run(); err != nil {
+		return
+	}
 }
