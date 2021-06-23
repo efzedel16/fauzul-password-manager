539621d6ba4deb8ef49bdd7516e7f6ee3bc44944
efzedel16 <efzedel16@outlook.com>
2021-06-23 16:31:01 +0700

    Fix & comment DeletedAt in migration

--- server/migrate/migration.go
+++ server/migrate/migration.go
@@ -3,15 +3,15 @@ package migrate
 import "time"
 
 type User struct {
-	Id           int        `json:"id" gorm:"primaryKey"`
-	FullName     string     `json:"full_name"`
-	Email        string     `json:"email" gorm:"unique"`
-	PasswordHash string     `json:"password_hash"`
-	Address      string     `json:"address"`
-	CreatedAt    time.Time  `json:"created_at"`
-	UpdatedAt    time.Time  `json:"updated_at"`
-	DeletedAt    time.Time  `json:"deleted_at" gorm:"index"`
-	PassApp      []Password `gorm:"foreignKey:userId"`
+	Id           int       `json:"id" gorm:"primaryKey"`
+	FullName     string    `json:"full_name"`
+	Email        string    `json:"email" gorm:"unique"`
+	PasswordHash string    `json:"password_hash"`
+	Address      string    `json:"address"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	//DeletedAt    time.Time  `json:"deleted_at" gorm:"index"`
+	PassApp []Password `gorm:"foreignKey:UserId"`
 }
 
 type Password struct {
@@ -21,5 +21,5 @@ type Password struct {
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at" gorm:"index"`
+	//DeletedAt time.Time `json:"deleted_at" gorm:"index"`
 }
