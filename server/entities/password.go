8a695599f1601bc4e06967f269b04df7b3b1ba61
efzedel16 <efzedel16@outlook.com>
2021-06-23 16:31:29 +0700

    Comment DeletedAt in password entity

--- server/entities/password.go
+++ server/entities/password.go
@@ -3,11 +3,11 @@ package entities
 import "time"
 
 type Password struct {
-  Id        int       `json:"id" gorm:"primaryKey"`
-  UserId    int       `json:"user_id"`
-  Website   string    `json:"web"`
-  Password  string    `json:"password"`
-  CreatedAt time.Time `json:"created_at"`
-  UpdatedAt time.Time `json:"updated_at"`
-  DeletedAt time.Time `json:"deleted_at" gorm:"index"`
+	Id        int       `json:"id" gorm:"primaryKey"`
+	UserId    int       `json:"user_id"`
+	Website   string    `json:"web"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	//DeletedAt time.Time `json:"deleted_at" gorm:"index"`
 }
