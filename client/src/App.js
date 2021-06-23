1028accb3e563992e84988acb1cea95fb47faac2
efzedel16 <efzedel16@outlook.com>
2021-06-24 03:16:49 +0700

    Add PassApp route

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,19 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignUp from "./components/pages/SignUp";
 import SignIn from "./components/pages/SignIn";
 import Main from "./components/pages/Main";
+import PassApp from "./components/pages/PassApp";
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Switch>
         <Route component={SignUp} exact path="/signup" />
         <Route component={SignIn} exact path="/signin" />
+        <Route component={PassApp} exact path="/pass-app" />
         <Route component={Main} exact path="/" />
       </Switch>
     </Router>
   );
-}
+};
 
 export default App;
