b2da3b30909c9e229b24b2a229db31b124019f01
efzedel16 <efzedel16@outlook.com>
2021-06-23 23:03:18 +0700

    Create main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignUp from "./components/pages/SignUp";
 import SignIn from "./components/pages/SignIn";
+import Main from "./components/pages/Main";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Switch>
         <Route component={SignUp} exact path="/signup" />
         <Route component={SignIn} exact path="/signin" />
+        <Route component={Main} exact path="/" />
       </Switch>
     </Router>
   );
