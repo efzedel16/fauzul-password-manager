7938b72ed61ad0d0a78b99a2d33f758efd950654
Muhammad Fauzul Hanif <50539720+efzedel16@users.noreply.github.com>
2021-06-24 18:29:47 +0700

    Delete client directory

--- client/src/API/FauzulPasswordManager.js
+++
@@ -1,7 +0,0 @@
-import Axios from "axios";
-
-const API = Axios.create({
-  baseURL: "https://fauzul-password-manager.herokuapp.com",
-});
-
-export default API;
--- client/src/store/store.js
+++
@@ -1,7 +0,0 @@
-import thunk from "redux-thunk";
-import rootReducer from "./rootReducer";
-import { applyMiddleware, createStore } from "redux";
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
